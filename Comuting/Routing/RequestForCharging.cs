3934cc88cab0f1aa77c5bb8e126e1834d1b8b208
Fisseha Teju <fishteju@gmail.com>
2022-07-19 12:23:06 +0800

    Add files via upload

---
+++ Comuting/Routing/RequestForCharging.cs
@@ -0,0 +1,69 @@
+﻿using RR.Dataplane;
+using RR.Dataplane.NOS;
+using RR.Dataplane.PacketRouter;
+using RR.Intilization;
+using RR.Comuting.computing;
+using RR.Properties;
+using RR.RingRouting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace RR.Comuting.Routing
+{
+    public class RequestForCharging
+    {
+        private LoopMechanizimAvoidance loopMechan = new LoopMechanizimAvoidance();
+        private NetworkOverheadCounter counter;
+        public BaseStation BStation { get; set; }
+
+        public RequestForCharging(Sensor sourceNode)
+        {
+            //counter = new NetworkOverheadCounter();
+
+            BStation = PublicParamerters.BS;
+            Packet Creq = GeneratePacket(sourceNode);
+            sendCHreqPacket(sourceNode, Creq);
+        }
+
+        //public RequestForCharging()
+        //{
+        //    counter = new NetworkOverheadCounter();
+        //    BStation = PublicParamerters.BS;
+        //}
+
+        private void sendCHreqPacket(Sensor sender, Packet pack)
+        {
+            ReceiveCHreqPacket(pack); // assuming the request received bay the a node close to the baseStation
+        }
+        private void ReceiveCHreqPacket(Packet pack)
+        {
+            BStation.SaveToQueue(pack);
+        }
+
+        private Packet GeneratePacket(Sensor sender)
+        {
+            //This is for the charging request
+            PublicParamerters.NumberofGeneratedChargeReqPackets += 1;
+            Packet pck = new Packet();
+            pck.Source = sender;
+            pck.Path = "" + sender.ID;
+            pck.Destination = null;
+            pck.DestinationPoint = BStation.Position;
+            pck.PacketType = PacketType.ChargingRequest;
+            pck.PID = PublicParamerters.NumberofGeneratedChargeReqPackets;
+            pck.remainingEnergy_Joule = sender.ResidualEnergy;
+            pck.dataTransmiting_Rate = sender.transmittingRate;
+            
+            //pck.TimeToLive = Convert.ToInt16((Operations.DistanceBetweenTwoPoints(sender.CenterLocation, pck.DestinationPoint) / (PublicParamerters.CommunicationRangeRadius / 3)));
+            //pck.TimeToLive += PublicParamerters.HopsErrorRange;
+            //counter.DisplayRefreshAtGenertingPacket(sender, PacketType.ChargingRequest);
+            return pck;
+
+        }
+    }
+}
---
+++ Dataplane/Charger.xaml.cs
@@ -0,0 +1,184 @@
+﻿using RR.Computations;
+using RR.Dataplane.PacketRouter;
+using RR.Intilization;
+using RR.Comuting.Routing;
+using RR.Models.Mobility;
+using RR.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
+using RR.Dataplane.NOS;
+
+namespace RR.Dataplane
+{
+    /// <summary>
+    /// Interaction logic for Charger.xaml
+    /// </summary>
+    public partial class Charger : UserControl
+    {
+        private static DispatcherTimer timer_move = new DispatcherTimer();
+        private static DispatcherTimer timer_Recharging = new DispatcherTimer();
+        private static DispatcherTimer timer_checkSinkGoingOut = new DispatcherTimer();
+        private static DispatcherTimer timer_getNewDirection = new DispatcherTimer();
+        public BaseStation Bstation { get; set; }
+        public Queue<Packet> Requests;
+        public List<Packet> packets_in_order = new List<Packet>();
+        public int ID { get; set; }
+        public bool isFree { get; set; }
+        public bool isgoingBack { get; set; }
+        public double ResidualEnergy { get; set; }
+
+        //
+        private Point depotPoint { get; set; }
+        private Point next_position { get; set; }
+        private Point destination { get; set; }
+        public Point Position
+        {
+            get
+            {
+                double x = Margin.Left;
+                double y = Margin.Top;
+                Point p = new Point(x, y);
+                return p;
+            }
+            set
+            {
+                Point p = value;
+                Margin = new Thickness(p.X, p.Y, 0, 0);
+            }
+        }
+
+        public Charger()
+        {
+            InitializeComponent();
+            int id = 1;
+            //lbl_charger_id.Text = id.ToString();
+            ID = id;
+            isFree = true;
+            ResidualEnergy = 100;
+            setPosition();
+
+            //Bstation = PublicParamerters.BS;
+
+        }
+        /// <summary>
+        /// Mobile charger tour starts here.
+        /// </summary>
+        /// <param name="task"></param>
+        public void startTour(Queue<Packet> Pack)
+        {
+            Requests = Pack;
+            isFree = false;
+            isgoingBack = false;
+            depotPoint = Position;
+            if (Requests.Count > 0 && Requests != null)
+            {
+                destination = Requests.Peek().Source.CenterLocation;
+            }
+            else
+            {
+                destination = Bstation.Position;
+                isgoingBack = true;
+                Requests.Clear();
+            }
+
+            timer_move.Interval = TimeSpan.FromSeconds(0.1);
+            timer_move.Start();
+            timer_move.Tick += MC_moving;
+
+        }
+        private double Rechargingtime = 3;
+        private int count = 0;
+        private void MC_moving(Object sender, EventArgs e)
+        {
+
+            if (Operations.DistanceBetweenTwoPoints(Position, destination) < 5)
+            {
+
+                if (destination != Bstation.Position)
+                {
+                    timer_move.Stop();
+
+                    // start recharging
+                    //Thread.Sleep(2000);
+
+                    timer_Recharging.Interval = TimeSpan.FromSeconds(Rechargingtime);
+                    timer_Recharging.Start();
+                    timer_Recharging.Tick += Recharging;
+
+                }
+                else
+                {
+                    PublicParamerters.BatteryIntialEnergyForMC = 500;
+                    endTour();
+                    return;
+                }
+
+                //timer_move.Stop();
+                //startTour(myTask);
+            }
+            next_position = get_NextPosition();
+            Position = next_position;
+        }
+
+        private void Recharging(Object sender, EventArgs e)
+        {
+            count++;
+            if (count >= Rechargingtime && Requests.Count > 0 && Requests != null)
+            {
+                timer_Recharging.Stop();
+                count = 0;
+
+                Requests.Peek().Source.ResidualEnergy = PublicParamerters.BatteryIntialEnergy;
+                PublicParamerters.requestedList.Remove(Requests.Dequeue().Source);
+
+                startTour(Requests); // go to the next node
+            }
+            else if (Requests.Count > 0 && Requests != null)
+            {
+                System.Console.WriteLine("Still recharging sensor {0}", Requests.Peek().Source.ID);
+            }
+
+        }
+        private Point get_NextPosition()
+        {
+            double EDs = Operations.DistanceBetweenTwoPoints(Position, destination);
+            if (EDs == 0) EDs = 1;
+            double Next_X = Position.X + (destination.X - Position.X) / EDs;
+            double Next_Y = Position.Y + (destination.Y - Position.Y) / EDs;
+            Point nextPoint = new Point(Next_X, Next_Y);
+
+            return nextPoint;
+        }
+
+        private void endTour()
+        {
+            if (Operations.DistanceBetweenTwoPoints(Position, Bstation.Position) < PublicParamerters.CommunicationRangeRadius / 10)
+            {
+                ResidualEnergy = 100;
+                isFree = true;
+            }
+            timer_move.Stop();
+        }
+
+        public void setPosition()
+        {
+            //Point pt = PublicParamerters.MainWindow.myNetWork[1].CenterLocation;
+            Position = new Point(PublicParamerters.MainWindow.Canvas_SensingFeild.ActualWidth / 2, PublicParamerters.MainWindow.Canvas_SensingFeild.ActualHeight / 2);
+        }
+    }
+}
