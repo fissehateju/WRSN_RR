100f7a7ef94c5b6fcc2551f131a3ecde69736362
Fisseha Teju <fishteju@gmail.com>
2022-08-01 02:01:59 +0800

    Add files via upload

---
+++ Models/Charging/Metrics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RR.Models.Charging
+{
+    public class Metrics
+    {
+        public double remainEnergy;
+        public double TrasmitRate;
+        public double distFromMC;
+        public double distFromAvgLocation;
+        
+        public Metrics(double remainEnergy, double trasmitRate, double distFromMC, double distFromAvgLocation)
+        {
+            this.remainEnergy = remainEnergy;
+            this.TrasmitRate = trasmitRate;
+            this.distFromMC = distFromMC;
+            this.distFromAvgLocation = distFromAvgLocation;
+        }
+
+    }
+}
